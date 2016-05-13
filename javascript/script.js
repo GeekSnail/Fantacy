0ee53ea0a15d3234c6f8854da66415f4f6029913
geeksnail <limingze6569@gmail.com>
2016-05-14 05:21:43 +0800

    hamburger pages commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
   window.onscroll=function(){
       var top=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
       var node=document.getElementById('outer');
-      if(top>99){//20就是滚动条滚动到的位置，大于20才显示
+      if(top>96){//20就是滚动条滚动到的位置，大于20才显示
           node.style.display='';
       }else{
           node.style.display='none';
