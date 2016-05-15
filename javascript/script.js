fd068ec9ec53bff55528362a2a39e8c4bd11ff71
geeksnail <limingze6569@gmail.com>
2016-05-15 22:44:42 +0800

    Scroll pages commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-  window.onscroll=function(){
+/*  window.onscroll=function(){
       var top=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
       var node=document.getElementById('outer');
       if(top>96){//20就是滚动条滚动到的位置，大于20才显示
@@ -7,4 +7,45 @@
           node.style.display='none';
       }
   }
+ */
+   window.onscroll=function(){
+      var top=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
+      var node=document.getElementById('outer');
+      if(top>877){//20就是滚动条滚动到的位置，大于20才显示
+          node.style.background='rgba(0,0,0,0.35)';
+		  node.style.position='fixed';
+		  
+      }else{
+          node.style.background='rgba(0,0,0,0)';
+		  node.style.position='absolute';
+		  
+      }
+  }
   
+/*  
+  $(document).ready(function() {
+
+    // hide #back-top first
+    $("#back-top").hide();
+
+    // fade in #back-top
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+});
+*/
