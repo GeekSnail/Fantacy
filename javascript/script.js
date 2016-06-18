cc04039e507e9c437417fad750d192dfbda96087
geeksnail <limingze6569@gmail.com>
2016-06-19 07:07:42 +0800

    导航栏 pages commit

--- javascript/script.js
+++ javascript/script.js
@@ -11,47 +11,94 @@
    window.onscroll=function(){
       var top=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
       var node=document.getElementById('outer');
-      if(top>745){//20就是滚动条滚动到的位置，大于20才显示
-	  	  
-          node.style.background='rgba(0,0,0,0.7)';
+      if(top>740){//20就是滚动条滚动到的位置，大于20才显示
+	  	  node.className="outerfixed";
+          node.style.background='rgba(255,255,255,0.9)';
 		  node.style.position='fixed';
 		  node.style.height='30px';
-		  node.style.lineHeight='45%';
+		  node.style.lineHeight='70%';
 		  node.style.color="red";
 		  node.style.zIndex=2010;//优先层叠，数字越大，越上方
-		  
+		  node.style.boxShadow='0 0 13px rgba(200,200,200,0.8)';
       }else{
+		  node.className="";
           node.style.background='rgba(0,0,0,0)';
 		  node.style.position='absolute';
-		  node.style.lineHeight='105%';
+		  node.style.lineHeight='100%';
 		  node.style.color='rgba(0,0,0,0.8)';
 		  node.style.zIndex=2010;
+		  node.style.boxShadow='';
       }
 	  var node=document.getElementById('nav1');
-      if(top>745){//20就是滚动条滚动到的位置，大于20才显示
+      if(top>740){//20就是滚动条滚动到的位置，大于20才显示
 	  	  lineHeight='20%';
           node.style.margin="-10px auto auto auto";
 		  
       }else{
-		  node.style.margin="0px auto auto auto";
+		  node.style.margin="-4px auto auto auto";
       }
 	  var node=document.getElementById('login-area');
-      if(top>745){//20就是滚动条滚动到的位置，大于20才显示
-	  	  lineHeight='20%';
-          node.style.margin="-9px auto auto auto";
+      if(top>740){//20就是滚动条滚动到的位置，大于20才显示
+	  	  /*lineHeight='20%';*/
+          node.style.margin="-10px auto auto auto";
 		  
       }else{
-		  node.style.margin="0px auto auto 5%";
+		  node.style.margin="-5px auto auto 5%";
       }
-	 /* var node=document.getElementByClassName("textcolor");
-      if(top>745){
+	  
+	  var lab = document.getElementsByTagName("a");//获取超链接对象数组
+      if(top>740){
 	  	  
-		  node.style.color="red";
-		  
-      }else{
-		 node.style.color='rgba(0,0,0,0)';
-		  
-      }*/
+/*		   for(var i=1;i<9;i++)  
+ 			{
+				   lab[i].className="navcol";
+				   if(i==2)
+				   {
+						 lab[i].className="menu-icon";  
+				   }
+			 }*/
+			 for(var i=1;i<9;i++)  
+ 			{
+				   lab[i].className="navcol";
+					if(i==2)
+				   {
+						 continue;  
+				   }	
+			 } 
+		  lab[2].className="menu-icon"; 
+      }else {
+		 	/*for(var i=1;i<9;i++)  
+ 			{
+				if(i==2)
+				   {
+						 lab[i].className="menu-ctrl";  
+				   }
+				  lab[i].className="";
+			 }*/
+		  for(var i=1;i<9;i++)  
+ 			{
+				if(i==2)
+				   {
+						 continue;  
+				   }
+				  lab[i].className="";
+			 }
+			 lab[2].className="menu-ctrl";
+      }
+	  var top1fix = document.getElementsByTagName("ul");//获取超链接对象数组
+      if(top>740){
+		  top1fix[0].className="top1fixed"; 
+		}else {
+			top1fix[0].className="";
+		}
+/*	  var node=document.getElementByClassName('menu-ctrl');
+      if(top>740){
+	  	 
+
+	  }else {
+	  }*/
+	  
+	  
 	/*  var node=document.getElementById('sidebar-container');
       if(top>895){//20就是滚动条滚动到的位置，大于20才显示
 		  node.style.position='fixed';
@@ -64,12 +111,12 @@
 	  	  
 		  node.style.display='';
 		 /* node.style.color='rgba(0,0,0,0.6)';*/
-		  node.style.zIndex=2010;
+		  node.style.zIndex=1;
 		  
       }else{
 		  node.style.display='none';
 		 /*node.style.color='rgba(0,0,0,0)';*/
-		 node.style.zIndex=2010;
+		 node.style.zIndex=0;
 		  
       }
 	  
@@ -135,6 +182,9 @@ WB2.anyWhere(function(W){
            img.width = imgW/2;
        }
   }
+  
+  
+ 
   /*$(function(){$("[data-toggle='tooltip']").tooltip()}),$(document).ready(function(){$("table").wrap("<div class='table-responsive'></div>"),$("table").addClass("table")}),$(document).ready(function(){$('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>'),$('iframe[src*="youtube.com"]').addClass("embed-responsive-item"),$('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>'),$('iframe[src*="vimeo.com"]').addClass("embed-responsive-item")}),jQuery(document).ready(function(a){var b=970;if(a(window).width()>b){var c=a(".navbar-custom").height();a(window).on("scroll",{previousTop:0},function(){var b=a(window).scrollTop();b<this.previousTop?b>0&&a(".navbar-custom").hasClass("is-fixed")?a(".navbar-custom").addClass("is-visible"):a(".navbar-custom").removeClass("is-visible is-fixed"):(a(".navbar-custom").removeClass("is-visible"),b>c&&!a(".navbar-custom").hasClass("is-fixed")&&a(".navbar-custom").addClass("is-fixed")),this.previousTop=b})}});*/
 /*   window.onscroll=function(){
       var top=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
